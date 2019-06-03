926c489b68aa25caa906ffff0aa3083ec321277e
C&J <C&J@DESKTOP-ILQ5FKQ>
2019-06-03 14:35:15 +0800

    4444

--- sfgdsg/src/main/java/sfgdsg/fsaf.java
+++ sfgdsg/src/main/java/sfgdsg/fsaf.java
@@ -3,6 +3,7 @@ package sfgdsg;
 public class fsaf {
 	public static void main(String[] args) {
 		System.out.println(125505555);
+		System.out.println(66556);
 	}
-        //5155555
+        //5155
 }
